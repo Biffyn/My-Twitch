c6373e0af11ebaee5e6fa6f97cd0e11669b72d52
biffyn <nuravenweb@gmail.com>
2016-09-30 14:23:51 +0200

    Basic UI

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
-});     //end doc.ready
+    $('.custom.example .ui.embed').embed({
+        source: 'youtube',
+        id: 'O6Xo21L0ybE',
+        placeholder: '/images/bear-waving.jpg'
+    });
+}); //end doc.ready
