c62c6809ee48663b0e8d1c66aa34512889ecf448
biffyn <nuravenweb@gmail.com>
2016-10-01 23:45:54 +0200

    Added user array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    
+    var userNames = [       // array of streamer
+        "ESL_SC2",
+        "OgamingSC2",
+        "cretetion",
+        "freecodecamp",
+        "storbeck",
+        "habathcx",
+        "RobotCaleb",
+        "noobs2ninjas"
+    ],
+    stram_url = "https://api.twitch.tv/kraken/streams/",
+    user_url = "https://api.twitch.tv/kraken/users/";
+
+
+    // make an ajax call to the twitch api
     $.ajax({
         type: 'GET',
         url: 'https://api.twitch.tv/kraken/channels/twitch',
@@ -8,5 +24,5 @@ $(document).ready(function() {
         success: function(data) {
             console.log(data);
         }
-    });
+    }); //end ajax call
 }); //end doc.ready
