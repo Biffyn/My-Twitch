f1466430cb1e47084e593757315f6255348a189e
biffyn <nuravenweb@gmail.com>
2016-10-02 19:04:30 +0200

    Added for loop to get data for all streamers

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
-    
-    var userNames = [       // array of streamer
-        "ESL_SC2",
-        "OgamingSC2",
-        "cretetion",
-        "freecodecamp",
-        "storbeck",
-        "habathcx",
-        "RobotCaleb",
-        "noobs2ninjas"
-    ],
-    stram_url = "https://api.twitch.tv/kraken/streams/",
-    user_url = "https://api.twitch.tv/kraken/users/";
+
+    var streamer = [ // array of streamer
+            "ESL_SC2",
+            "OgamingSC2",
+            "cretetion",
+            "freecodecamp",
+            "storbeck",
+            "habathcx",
+            "RobotCaleb",
+            "noobs2ninjas"
+        ],
+        stram_url = "https://api.twitch.tv/kraken/streams/",
+        user_url = "https://api.twitch.tv/kraken/users/";
 
 
-    // make an ajax call to the twitch api
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.twitch.tv/kraken/channels/twitch',
-        headers: {
-            'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
-        },
-        success: function(data) {
-            console.log(data);
-        }
-    }); //end ajax call
+    // make an ajax call to the twitch api for each of the streamers in the array
+    streamer.forEach(function(val) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.twitch.tv/kraken/streams/' + val,
+            headers: {
+                'Client-ID': 'n9ilovhcd0k79pt99zkmffzp0au9gw1'
+            },
+            success: function(res) {
+                    console.log(res);
+                } // end response
+        }); //end ajax call
+    }); // end forEach
 }); //end doc.ready
