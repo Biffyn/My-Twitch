a626fc8b904a579537dd1dddfbde7fa38dcf06cf
biffyn <nuravenweb@gmail.com>
2016-10-05 06:34:44 +0200

    Added display offline and missing channels function, some styling changes

--- js/app.js
+++ js/app.js
@@ -1,57 +1,34 @@
-$(document).ready(function() {
-
-  var streamer = [ // array of streamer
-      "ESL_SC2",
-      "OgamingSC2",
-      "cretetion",
-      "freecodecamp",
-      "storbeck",
-      "habathcx",
-      "RobotCaleb",
-      "noobs2ninjas"
-    ],
-    stream_url = 'https://api.twitch.tv/kraken/streams/',
-    channel_url = 'https://api.twitch.tv/kraken/channel/',
-    clientID = '?client_id=n9ilovhcd0k79pt99zkmffzp0au9gw1&callback=?';
-
-  // make an ajax call to the twitch api for each of the streamers in the array
-  streamer.forEach(function(val) {
-    $.ajax({
-      type: 'GET',
-      url: stream_url + val,
-      headers: {
-        'Client-ID': 'n9ilovhcd0k79pt99zkmffzp0au9gw1'
-      },
-      success: function(res) {
-          console.log(res);
-          if (res.stream != null) {
-            $('#online').show();
-            $('#online').append(`<div class="col-xs-12 col-md-4">
-                                <div id="card" class="ui centered card">
-                                    <a class="image" href="` + res.stream.channel.url + `" target="_blank" rel="noopener noreferrer">
-                                        <img src="` + res.stream.preview.large + `">
-                                    </a>
-                                    <div class="content">
-                                        <a class="header" href="` + res.stream.channel.url + ` " target="_blank" rel="noopener noreferrer">` + res.stream.channel.display_name + `</a>
-                                        <div class="description"> ` + res.stream.channel.status.substring(0, 40) + ` ... </div>
-                                    </div>
-                                    <div class="extra content">
-                                        <span class="right floated">
-                                            Online
-                                        </span>
-                                        <span>
-                                        <i class="user icon"></i>
-                                            ` + res.stream.viewers + ` Viewers
-                                      </span>
-                                    </div>
-                                </div>`);
-          } else if (res.stream === null) {
-            $('#offline').show();
-
-          } else if (error) {
-
-          }
-        } // end response
-    }); //end ajax call
-  }); // end forEach
-}); //end doc.ready
+$(document).ready(function () {
+    var streamer = [
+            'ESL_SC2',
+            'OgamingSC2',
+            'nonplayercat',
+            'cretetion',
+            'freecodecamp',
+            'riotgames',
+            'luxxbunny',
+            'narcosvszombies',
+            'bobross',
+            'esl_csgo',
+            'Nightblue3',
+            'PhantomL0rd',
+            'noobs2ninjas'
+        ], stream_url = 'https://api.twitch.tv/kraken/streams/', user_url = 'https://api.twitch.tv/kraken/users/', clientID = '?client_id=n9ilovhcd0k79pt99zkmffzp0au9gw1&callback=?';
+    streamer.forEach(function (val) {
+        $.getJSON(stream_url + val + clientID, function (res) {
+            console.log(res);
+            if (res.stream != null) {
+                $('#online').show();
+                $('#online').append('<div class="col-xs-12 col-md-4">\n<div id="card" class="ui centered card">\n<a class="image" href="' + res.stream.channel.url + '" target="_blank" rel="noopener noreferrer">\n<img src="' + res.stream.preview.large + '">\n</a>\n<div class="content">\n<a class="header" href="' + res.stream.channel.url + ' " target="_blank" rel="noopener noreferrer">' + res.stream.channel.display_name + '</a>\n<div class="meta">' + res.stream.channel.game + '</div>\n<div class="description"> ' + res.stream.channel.status.substring(0, 35) + ' ... </div>\n</div>\n<div class="extra content">\n<span class="right floated">\n<i id="blink" class="circle green icon"></i>\nOnline\n</span>\n<span>\n<i class="user icon"></i>\n' + res.stream.viewers + ' Viewers\n</span>\n</div>\n</div>');
+            } else if (res.stream === null) {
+                $.getJSON(res._links.channel + clientID, function (data) {
+                    $('#offline').show();
+                    $('#offline').append('<div class="col-xs-12 col-md-4">\n<div id="card" class="ui centered card">\n<a class="image" href="' + data.url + '" target="_blank" rel="noopener noreferrer">\n<img src="' + data.video_banner + '">\n</a>\n<div class="content">\n<a class="header" href="' + data.url + ' " target="_blank" rel="noopener noreferrer">' + data.display_name + '</a>\n<div id="bio" class="description"> Hello </div>\n</div>\n<div class="extra content">\n<span class="right floated">\n<i class="circle red icon"></i> \nOffline\n</span>\n<span>\n<i class="user icon"></i>\n' + data.followers + ' Followers\n</span>\n</div>\n</div>');
+                });
+            } else if (res.status === 422) {
+                $('#deleted').show();
+                $('#deleted').append('<h1 class="text-center dark">Deleted or Missing</h1>\n<div class="col-xs-12 col-md-4">\n<div id="card" class="ui centered card">\n<a class="image" href="href="https://giphy.com/gifs/gngO1gmBhS9na" target="_blank" rel="noopener noreferrer">\n<img src="http://i.giphy.com/gngO1gmBhS9na.gif">\n</a>\n<div class="content">\n<div class="description"> ' + res.message + ' </div>\n</div>\n<div class="extra content">\n<span class="right floated">\n<i class="circle red icon"></i> \nLost in space and time\n</span>\n<div>\n</div>');
+            }
+        });
+    });
+});
